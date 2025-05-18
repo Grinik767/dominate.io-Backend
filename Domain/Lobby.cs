8912c2264d02c6458a82b13997f8e08660d6c3e5
Grinik767 <grigorikiseleff@yandex.ru>
2025-05-18 13:43:23 +0500

    continue

--- Domain/Lobby.cs
+++ Domain/Lobby.cs
@@ -1,10 +1,33 @@
-﻿namespace Domain;
+﻿using System.Collections.Concurrent;
+using System.Data;
 
-public class Lobby(Player[] players)
+namespace Domain;
+
+public class Lobby
 {
     public readonly State Situation = new();
     public readonly Player[] Players = new Player[4];
-    public readonly Player Leader = players[0];
-
+    public readonly Player Leader;
     public DateTime LastAccess = DateTime.Now;
+
+    private readonly ConcurrentDictionary<Guid, Player> _data = new();
+
+    public Lobby(string leaderNickname)
+    {
+        var leader = new Player(leaderNickname, GetFreeColor());
+        
+        _data[Guid.NewGuid()] = leader;
+        Leader = leader;
+    }
+
+    private Color GetFreeColor()
+    {
+        var allColors = Enum.GetValues(typeof(Color)).Cast<Color>().ToHashSet();
+        allColors.ExceptWith(_data.Values.Select(player => player.Color).ToHashSet());
+
+        if (allColors.Count == 0)
+            throw new DataException("No free color available");
+
+        return allColors.First();
+    }
 }
--- Infrastructure/Storage.cs
+++ Infrastructure/Storage.cs
@@ -1,6 +1,11 @@
-﻿namespace Infrastructure;
+﻿using System.Collections.Concurrent;
+using Domain;
+
+namespace Infrastructure;
 
 public class Storage
 {
+    private readonly ConcurrentDictionary<string, Lobby> _data = new();
+    
     
 }
