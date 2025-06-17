b69b36164b88482d91b5f459e552c725a8fe0758
Cyecaid <loginmish@gmail.com>
2025-06-17 20:27:41 +0500

    forgotten part

---
+++ Application/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+
+namespace Application;
+
+public interface ICommand
+{
+    string Type { get; }
+    Task ExecuteAsync(Lobby lobby, string lobbyCode, string nickname, IConnectionService connectionService, CancellationToken ct);
+}
